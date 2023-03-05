b6365fdad3b2481a4052306cdc67abb13a63d481
irenee-14 <ireneee145@gmail.com>
2023-03-05 22:34:32 +0900

    signal handling

--- srcs/main.c
+++ srcs/main.c
@@ -3,35 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joyoo <joyoo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jihylim <jihylim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 19:23:46 by jihylim           #+#    #+#             */
-/*   Updated: 2023/03/05 19:11:55 by joyoo            ###   ########.fr       */
+/*   Updated: 2023/03/05 22:33:00 by jihylim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	signal_c(int signal)
+void	signal_prompt(int signal)
 {
-	if (signal == SIGINT)
-	{
-		write(1, PROMPT, ft_strlen(PROMPT));
-		write(1, "\n", 1);
-	}
+	(void)signal;
+	g_status = 130;
+	// write(1, PROMPT, ft_strlen(PROMPT));
+	write(1, "\n", 1);
 	// 현재까지 입력된 문자열을 str로 바꿔주는 함력
 	// 프롬포트를 입력하지 않고 새로운 프롬포트 출력할 때, 프롬포트 비워주기
-	rl_replace_line("", 1);
+	rl_replace_line("", 0);
 	// rl_display 실행 위해 필요
 	rl_on_new_line();
 	// readline에 입력된 문자열 다시 출력
 	rl_redisplay();
 }
 
-void	signal_setting(void)
+void	signal_exe(int signal)
 {
-	signal(SIGINT, signal_c);
-	signal(SIGQUIT, SIG_IGN);
+	(void)signal;
+	g_status = 130;
+	write(1, "^C", 2);
+	write(1, "\n", 1);
+	// kill(getpid(), SIGCHLD);
+}
+
+void	signal_slash(int signal)
+{
+	(void)signal;
+	g_status = 130;
+	ft_putstr_fd("^\\Quit: 3\n", 1);
+	// kill(0, SIGKILL);
+}
+
+void	signal_setting(int flag)
+{
+	if (flag == 1)
+	{
+		signal(SIGINT, signal_prompt);
+		signal(SIGQUIT, SIG_IGN);
+	}
+	else if (flag == 2)
+	{
+		signal(SIGINT, signal_exe);
+		signal(SIGQUIT, signal_slash);
+		signal(SIGTERM, signal_exe);
+	}
 }
 
 t_list	*test_parsing(void)
@@ -59,10 +84,10 @@ int	main(int ac, char **av, char **envp)
 	tcgetattr(STDIN_FILENO, &term);
 	term.c_lflag &= ~(ECHOCTL);
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	signal_setting();
 	make_env(&env, envp);
 	while (1)
 	{
+		signal_setting(1);
 		line = readline(PROMPT);
 		if (!line)
 		{
@@ -80,7 +105,8 @@ int	main(int ac, char **av, char **envp)
 			mini_list = parsing(line, env);
 			free(line);
 			if (mini_list)
-			{					
+			{
+				signal_setting(2);
 				tmp = mini_list;
 				while (tmp)
 				{
@@ -94,7 +120,7 @@ int	main(int ac, char **av, char **envp)
 				ft_lstclear_mini(&mini_list);
 			}
 		}
-		printf("g_status: %d\n", g_status);
+		// printf("g_status: %d\n", g_status);
 	}
 	(void)ac;
 	(void)av;
