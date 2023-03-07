40ad42bfc47cf780fe17fe06e3c87dcb3f8d3a54
irenee-14 <ireneee145@gmail.com>
2023-03-07 18:10:22 +0900

    ctrl + c exit 코드 저장

--- srcs/execute.c
+++ srcs/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joyoo <joyoo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jihylim <jihylim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 13:21:42 by joyoo             #+#    #+#             */
-/*   Updated: 2023/03/05 18:28:08 by joyoo            ###   ########.fr       */
+/*   Updated: 2023/03/07 18:09:27 by jihylim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,10 @@ int	execute(t_list *mini_list, t_list **env)
 			else
 			{
 				waitpid(pid, &g_status, 0);
-				g_status = (g_status & 0xff00) >> 8;
+				if (g_status == 2 || g_status == 3)
+					g_status = 128 + g_status;
+				else
+					g_status = (g_status & 0xff00) >> 8;
 				free_split(env_char);
 			}
 		}
