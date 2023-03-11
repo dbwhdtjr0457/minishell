6e8a016f7c4b904ccbd91c08deb1a7cc56913844
irenee-14 <ireneee145@gmail.com>
2023-03-11 20:25:01 +0900

    main norm

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jihylim <jihylim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 19:23:46 by jihylim           #+#    #+#             */
-/*   Updated: 2023/03/10 19:02:39 by jihylim          ###   ########.fr       */
+/*   Updated: 2023/03/11 20:24:20 by jihylim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	print_ascii(void)
 	int		fd;
 	char	*line;
 
-	fd = open("./docs/ascii.txt", O_RDONLY);
+	fd = open("./title/ascii.txt", O_RDONLY);
 	if (!fd)
 	{
 		printf("error\n");
@@ -35,61 +35,64 @@ void	print_ascii(void)
 	close(fd);
 }
 
-int	main(int ac, char **av, char **envp)
+void	init(t_list **env, char **envp)
 {
-	char			*line;
-	t_list			*mini_list;
-	t_list			*env;
-	t_list			*tmp;
-
 	print_ascii();
-	make_env(&env, envp);
+	make_env(env, envp);
 	set_signal(signal_prompt, SIG_IGN);
 	term_off();
+}
+
+void	main_loop(char *line, t_list **env)
+{
+	t_list	*tmp;
+	t_list	*mini_list;
+
+	add_history(line);
+	mini_list = parsing(line, *env);
+	free(line);
+	if (!mini_list)
+		return ;
+	tmp = mini_list;
+	while (tmp)
+	{
+		if (!check_heredoc(((t_mini *)tmp->content)->redir))
+			break ;
+		tmp = tmp->next;
+	}
+	if (!tmp)
+	{
+		term_on();
+		if (ft_lstsize(mini_list) > 1)
+			pipe_execute(mini_list, env);
+		else
+			execute(mini_list, env);
+		term_off();
+	}
+	ft_lstclear_mini(&mini_list);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	char	*line;
+	t_list	*env;
+
+	init(&env, envp);
 	while (1)
 	{
 		line = readline(PROMPT);
 		if (!line)
 		{
-			// ctrl + D 눌렀을 경우
-			// 한줄 올리고 커서 12 만큼 앞으로 해서 exit 출력하고 while 빠져나가기
 			ft_putstr_fd("\0338exit\n", 1);
 			break ;
 		}
-		//아무것도 입력 안하고 엔터만 쳤을 때, history 에 기록 안하도록
 		else if (*line == '\0')
 			free(line);
 		else
-		{
-			add_history(line);
-			mini_list = parsing(line, env);
-			free(line);
-			if (mini_list)
-			{
-				tmp = mini_list;
-				while (tmp)
-				{
-					if (!check_heredoc(((t_mini *)tmp->content)->redir))
-						break ;
-					tmp = tmp->next;
-				}
-				if (!tmp)
-				{
-					term_on();
-					if (ft_lstsize(mini_list) > 1)
-						pipe_execute(mini_list, &env);
-					else
-						execute(mini_list, &env);
-					term_off();
-				}
-				ft_lstclear_mini(&mini_list);
-			}
-		}
-		//printf("g_status: %d\n", g_status);
+			main_loop(line, &env);
 	}
 	(void)ac;
 	(void)av;
 	ft_lstclear_env(&env);
-	// system("leaks minishell");
 	return (0);
 }
